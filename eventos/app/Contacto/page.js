fe6e7161103c62c35240557882fe5ff262210607
DANA LEILA MANDELBAUM <47574606@ort>
2024-09-19 09:06:43 -0300

    Eventos

--- eventos/app/Contacto/page.js
+++ eventos/app/Contacto/page.js
@@ -1,18 +1,26 @@
-import styles from './module.css'; 
+import styles from './styles.module.css'; 
+import Ubicacion from '../assets/Img/Ubicacion.png'
+import Image from 'next/image';
 
 const Contacto = () => {
+  const IMG_SIZE = 500;
   return (
-    <>
-      <main className={styles.main}>
+    <div>
         <h1 className={styles.title}>Contacto</h1>
         <p className={styles.info}>Información de contacto:</p>
-        <address className={styles.address}>
-          <p>Dirección: Yatay 240</p>
-          <p>Teléfono: 123-456-7890</p>
-          <p>Email: escuela@ort.com</p>
-        </address>
-      </main>
-    </>
+        <div className={styles.container}>
+          <address className={styles.address}>
+            <p>Dirección: Yatay 240</p>
+            <p>Teléfono: 123-456-7890</p>
+            <p>Email: escuela@ort.com</p>
+          </address>
+          <Image 
+            src={Ubicacion}
+            width={IMG_SIZE}
+            height={IMG_SIZE}
+          />
+        </div>
+    </div>
   );
 };
 
--- eventos/app/Eventos/page.js
+++ eventos/app/Eventos/page.js
@@ -1,16 +1,40 @@
+"use client";
 
+import React, { useEffect, useState } from 'react';
+import './styles.module.css'; 
+import ListarEvento from '../components/ListarEvento';
 
 const Eventos = () => {
+  const [eventos, setEventos] = useState([]);
+
+  useEffect(() => {
+    setEventos([
+      {
+        nombre: 'Deportes',
+        precio: 434,
+        duration: 45454,
+      },
+      {
+        nombre: 'Amiguismo',
+        precio: 434,
+        duration: 45454,
+      }
+    ]);
+  }, []);
+
   return (
-    <>
-      <main>
-        <h1>Listado de Eventos</h1>
-        <ul>
-          <li><a href="./Detalle-eventos">Evento 1</a></li>
-          <li><a href="./Detalle-eventos">Evento 2</a></li>
-        </ul>
-      </main>
-    </>
+    <main className="eventos-container">
+      <h1 className="titulo">Listado de Eventos</h1>
+      {
+        eventos.map(e => (
+          <ListarEvento
+            key={e.nombre} 
+            nombre={e.nombre}
+            precio={e.precio}
+          />
+        ))
+      }
+    </main>
   );
 };
 
